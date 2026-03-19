0eb1cd946ddbf2b52fba3adf4380139f839beb44
Berthold <berthnk@gmail.com>
2026-03-19 02:30:19 +0100

    fixed build errors

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Suspense } from "react";
 import { Sidebar } from "./sidebar";
 import { TopNavbar } from "./top-navbar";
 
@@ -10,9 +11,13 @@ interface MainLayoutProps {
 export function MainLayout({ children }: MainLayoutProps) {
   return (
     <div className="h-dvh bg-brand/5 flex overflow-hidden">
-      <Sidebar />
+      <Suspense fallback={<div className="w-64 border-r border-border bg-sidebar" />}>
+        <Sidebar />
+      </Suspense>
       <div className="flex-1 min-w-0 flex flex-col overflow-hidden">
-        <TopNavbar />
+        <Suspense fallback={<div className="h-12 border-b border-border bg-background" />}>
+          <TopNavbar />
+        </Suspense>
         <main className="flex-1 min-w-0 w-full overflow-y-auto pb-16 md:pb-0" style={{ scrollbarGutter: "stable" }}>
           <div className="w-full h-full mx-auto p-4 md:p-8 flex flex-col">
             {children}
